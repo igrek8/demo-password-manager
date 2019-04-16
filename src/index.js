54695569b1709195c36025f9ab4eae37892976c4
Igor Korchagin <i.korchagin@igrec.local>
2019-04-17 02:06:57 +0300

    применим reset.css и добавим роутер

--- src/index.js
+++ src/index.js
@@ -1,5 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import { BrowserRouter as Router } from 'react-router-dom';
 import App from './components/app';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <Router>
+    <CssBaseline>
+      <App />
+    </CssBaseline>
+  </Router>,
+  document.getElementById('root'),
+);
